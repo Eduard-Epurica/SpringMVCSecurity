822957d991df2c9b4789ce80a516720fe4ba3512
Eduard Epurica <epurica.eduard@gmail.com>
2023-10-20 16:27:45 +0300

    Added support for JDBC

--- src/main/java/com/eduard/mvcsecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/eduard/mvcsecurity/security/DemoSecurityConfig.java
@@ -3,37 +3,23 @@ package com.eduard.mvcsecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+
+    // add support for JDBC .... no more hardcoded users
+    // the following method injects the dataSource that is autoconfigured by sping boot
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,susan,mary);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+            return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -58,9 +44,34 @@ public class DemoSecurityConfig {
                 .exceptionHandling(configurer->configurer.accessDeniedPage("/access-denied")
                 );
 
-
-
         return http.build();
     }
+
+    /*
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(john,susan,mary);
+    }
+    */
+
 
 }
